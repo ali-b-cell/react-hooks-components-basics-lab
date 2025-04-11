14a71d66ba24d3c28f7fb1c7c9b4ff51a0684c18
alice <alice.nthenge@student.moringaschool.com>
2025-04-11 07:42:10 +0300

    completed lab with modified file

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,36 @@
 import React from "react";
-
 function NavBar() {
   return (
     <nav>
-      <a href="#home">I'm a link!</a>
+      <h1>My portfolio</h1>
     </nav>
   );
 }
 
 function Home() {
   return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
+    <main id="home">
+      <h2>Welcome to my site</h2>
+    </main>
   );
 }
 
-{/* write an <About> component here */}
+
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  );
+}
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
